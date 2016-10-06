69b102520824830cac11f4c95bd6ace44d8d5f44
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-06 17:43:05 +0100

    [ruby] added docs and example code

---
+++ ruby/example_code.rb
@@ -0,0 +1,26 @@
+require 'microservice_logging'
+require 'time'
+
+MY_EVENTS = [
+  :http_request,
+  :exception,
+  :startup,
+  :my_event,
+  :my_other_event
+]
+
+log = MicroserviceLogger.new(:service_name => 'my_super_service',
+                             :clock => Time,
+                             :output => $stdout,
+                             :events => MY_EVENTS
+                            )
+
+log.startup.error({ message: "Emergency! There's an Emergency going on" })
+
+puts "\n"
+
+log.http_request.info({
+  correlation_id: '6889828a-6708-46f7-beec-870cf7b4ab6f',
+  request: { method: 'GET' },
+  response: { status_code: 200 }
+})
